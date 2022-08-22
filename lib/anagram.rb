19e8850f0ffc11971cc0daad5f02d80c9a3065bf
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-22 16:32:38 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word="listen")
+        @word = word
+    end
+
+    def match(array)
+        array.filter{|word| word.chars.sort == @word.chars.sort}
+    end
+end
